6b2afb8a6c9aaafeef78252de072d70ed9fa5f0c
Hiromichi.Kasahara1 <Hiromichi.Kasahara1@jp.ricoh.com>
2025-09-05 07:56:40 +0900

    修正

--- ch09/ex04/index.js
+++ ch09/ex04/index.js
@@ -24,26 +24,26 @@ export class MagicWarrior_c extends Warrior_c {
 
 /* ----------------prototypeを使用----------------- */
 // 戦士クラス
-export function Warrior_p(atk) {
-  this.atk = atk;
+export function Warrior_p(atk) { // コンストラクタ関数
+  this.atk = atk; // atkを初期化
 }
 
-Warrior_p.prototype.attack = function() {
+Warrior_p.prototype.attack = function() { // Warrior_pのプロトタイプにattackメソッドを追加
   return this.atk * 2;
 };
 
 // 魔法戦士クラス
-export function MagicWarrior_p(atk, mgc) {
-  Warrior_p.call(this, atk); // Warrior_pのコンストラクタを呼び出す
-  this.mgc = mgc;
+export function MagicWarrior_p(atk, mgc) { // コンストラクタ関数
+  Warrior_p.call(this, atk); // Warrior_pのコンストラクタを呼び出してatkを初期化
+  this.mgc = mgc; // mgcを初期化
 }
 
 // プロトタイプ継承
-MagicWarrior_p.prototype = Object.create(Warrior_p.prototype);
-MagicWarrior_p.prototype.constructor = MagicWarrior_p;
+MagicWarrior_p.prototype = Object.create(Warrior_p.prototype); // Warrior_pのプロトタイプを継承
+MagicWarrior_p.prototype.constructor = MagicWarrior_p; // constructorがMagicWarrior_pを指すように修正
 
 // attackメソッドのオーバーライド
 MagicWarrior_p.prototype.attack = function() {
-  return Warrior_p.prototype.attack.call(this) + this.mgc;
+  return Warrior_p.prototype.attack.call(this) + this.mgc; // 親クラスのattackメソッドを呼び出し、mgcを加算
 };
 
